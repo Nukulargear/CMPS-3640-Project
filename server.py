da262984b9b06a08165ee3b5e04ca85e5b03c179
Sharadeos <shadow_zeny@hotmail.com>
2018-11-09 13:42:25 -0800

    Update server.py

--- server.py
+++ server.py
@@ -15,8 +15,8 @@ from _thread import *
 
 '''
 try:
-	connect_str = "dbname='oxysquare' user='pi' host='sharadeos.ddns.net' " + \
-				"password='Dol0Rubx'"
+	connect_str = "dbname='' user='' host='sharadeos.ddns.net' " + \
+				"password=''"
 	# use our connection values to establish a connection
 	conn = psycopg2.connect(connect_str)
 	# create a psycopg2 cursor that can execute queries
